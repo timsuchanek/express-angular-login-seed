ed6cae8a153f3d66447446352ef55da4cc764cbf
vardump <tim.suchanek@gmail.com>
2013-09-03 11:02:27 +0200

    Added html5 routing to Angular

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
- 
+
 var app = express();
+var privateArea = require('routes/privateArea');
 app.use(express.logger());
 
 // Configuration
@@ -18,6 +19,8 @@ app.configure(function(){
 app.get('/', function(request, response) {
   response.render('index.html')
 });
+
+app.get('/privateArea/', privateArea.index);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
