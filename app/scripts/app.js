70bdb147303a991fec036d5af398b0df0972ffd0
vardump <tim.suchanek@gmail.com>
2013-09-03 11:02:53 +0200

    Again html5 mode

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,18 @@
 'use strict';
 
 angular.module('generatorLoginApp', [])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/privateArea', {
+        templateUrl: 'views/privateArea.html',
+        controller: 'PrivateareaCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
+    $locationProvider.html5Mode(true);
   });
